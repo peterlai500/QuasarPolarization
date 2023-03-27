fb16e48f5e89ab4ca0bf7ea5742e2655d40359cc
peterlai500 <b092030012@g-mail.nsysu.edu.tw>
2023-03-27 19:25:38 +0800

    try to write QuasarPol.py as useful package

--- QuasarPol_Class.py
+++ QuasarPol_Class.py
@@ -11,11 +11,16 @@ from astroplan import Observer
 
 class QuasarPol:
     
+    def __init__(self, ):
+        self 
+        
+    
     def almaquery(source):
         result_ObsCorelist = alma.query(payload=dict(source_name_alme=source),
-                                        science=False)
+                                        science=False, 
+                                        maxrec=100)
 
         result_Almalist = alma.query(payload=dict(source_name_alme=source),
-                                        science=False,
-                                        lagacy_columns=True)
-
+                                     science=False,
+                                     lagacy_columns=True
+                                     maxrec=100)
